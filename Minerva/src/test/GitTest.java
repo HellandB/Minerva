892a3e0dae5f8294730099f555ed4f723317fb36
klikkamongo <helland.boe@gmail.com>
2012-09-10 16:48:29 +0200

    endrer teksten

--- Minerva/src/test/GitTest.java
+++ Minerva/src/test/GitTest.java
@@ -8,8 +8,9 @@ public class GitTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("testing Git");
+		System.out.println("testing test");
 		System.out.println ("linje nummer 2");
+		
 	}
 
 }
