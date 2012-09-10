cce38783b56ac9b9d2735007a89cf6113f21f4a8
klikkamongo <helland.boe@gmail.com>
2012-09-10 20:16:41 +0200

    Tester master branch, legger til ein kommentar på linje 11

--- Minerva/src/test/GitTest.java
+++ Minerva/src/test/GitTest.java
@@ -7,10 +7,12 @@ public class GitTest {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		// Master branch
 		System.out.println("testing testing");
 		System.out.println ("linje nummer 2");
 		System.out.println("Git Minerva is Online and updating!");
+	
 	}
 
 }
