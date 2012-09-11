35ad514652899748baf8c0cab9af84d16e88afb5
Andreas Nesheim <andreas.aronsen.nesheim@gmail.com>
2012-09-11 11:32:06 +0200

    Update Andreas

---
+++ Minerva/src/test/EkstraKlasse.java
@@ -0,0 +1,13 @@
+package test;
+
+public class EkstraKlasse {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hei på deg");
+	}
+
+}
