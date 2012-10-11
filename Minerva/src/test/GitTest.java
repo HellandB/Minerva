9b60cdb0de49235651b3658a2632583037a39aa7
Daniel Jajevski <daniel.jajevski@gmail.com>
2012-10-11 13:52:14 +0200

    fucked up 'GitTest.java'

--- Minerva/src/test/GitTest.java
+++ Minerva/src/test/GitTest.java
@@ -6,14 +6,15 @@ public class GitTest {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		
-		
 		// Master branch
 		System.out.println("testing testing");
 		System.out.println ("linje nummer 2");
 		System.out.println("Git Minerva is Online and updating!");
 		int lol = 0;
 		System.out.println(lol + "is working yet? Jokki");
+		
+		// the Master Himself
+		System.out.println("this is outta my league.\t- D.J.");
 	}
 
 }
